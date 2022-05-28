0f2afe8c762e451de39ee0a9133ca63c7660fa25
m-triple-m <triplem656@gmail.com>
2022-05-28 19:41:14 +0530

    ..

--- frontend/src/components/main/browseEquipment.js
+++ frontend/src/components/main/browseEquipment.js
@@ -1,6 +1,13 @@
 //import React from 'react'
 import { Search } from "@mui/icons-material";
-import { InputAdornment, Typography, Button } from "@mui/material";
+import {
+  InputAdornment,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import app_config from "../../config";
@@ -36,23 +43,26 @@ const BrowseEquipment = () => {
       return datalist.map(
         ({ title, description, image, features, price, seller, _id }) => (
           <div key={_id} class="col-md-12 col-lg-4 mb-4 mb-lg-0 ">
-            <div class="product">
-              <NavLink className="ripple" to={"/main/equipmentDetails/" + _id}>
-                <img
-                  src={url + "/uploads/" + image}
-                  class="card-img-top"
-                  alt="Laptop"
-                />
-              </NavLink>
-
-              <h1 class="product__title">{title}</h1>
-              <h3>₹ {price}</h3>
-              <hr />
-              {/* <p>The Nike Roshe One Print Men's Shoe offers breathability, lightweight cushioning and bold style with an injected unit midsole and colorful mesh upper. </p> */}
-              <a href="/main/equipmentDetails" class="product__btn btn">
-                Buy Now
-              </a>
-            </div>
+            <Card>
+              <CardMedia
+                component="img"
+                image={url + "/uploads/" + image}
+                height={400}
+              />
+              <CardContent className="card-body">
+                <p className="text-muted h3">{title}</p>
+                <h2 className="float-end">₹ {price}</h2>
+                <br />
+                <hr />
+                <Button
+                  variant="contained"
+                  className="mt-5 w-100"
+                  onClick={(e) => navigate("/main/payment")}
+                >
+                  Buy Now
+                </Button>
+              </CardContent>
+            </Card>
           </div>
         )
       );
@@ -63,35 +73,26 @@ const BrowseEquipment = () => {
 
   return (
     <div>
-      <div style={{ background: "#eee", height: "300vh" }}>
+      <div>
         <img
-          class="img-fluid"
+          class="w-100"
           src="https://cdn.shopify.com/s/files/1/2980/5140/files/desktop_banner_-_3_5a730f81-fb4f-4a4f-9910-727963303dfa_1600x.png?v=1627145304"
-        ></img>
-        <div class="row ground">
-          <div class="col-md-5 mb-3">
-            <div class="form-outline form">
-              {/* <label for="state" > </label> */}
-
-              {/* <input class="applyNowBtn" id="event-search" type="submit" name="searchevent" value="Search"></input> */}
-
-              {/* <a href="#" class="adSearchLink">Advance Search </a>   */}
-            </div>
-          </div>
+          alt=""
+        />
+        <div className="container-fluid">
+          <img
+            class="w-100"
+            src="https://cdn.shopify.com/s/files/1/2980/5140/collections/loom_solar_banner_1200x.jpg?v=1641970493"
+            alt=""
+          />
+          <div className="row mt-5">{displayData()}</div>
         </div>
         <div class="wrapper">
           <div class="header">
-            <img
-              class="solarimg"
-              src="https://cdn.shopify.com/s/files/1/2980/5140/collections/loom_solar_banner_1200x.jpg?v=1641970493"
-            ></img>
-
             {/* <h1 class="header__title">Expanding Card Grid</h1>
   <h2 class="header__subtitle">with Flexbox</h2> */}
 
-            <div className="container">
-              <div className="row">{displayData()}</div>
-            </div>
+            <div className="container"></div>
           </div>
         </div>{" "}
       </div>
--- frontend/src/components/main/payment.js
+++ frontend/src/components/main/payment.js
@@ -1,9 +1,14 @@
 import "../../stylesheets/payment.css";
 import { useEffect, useState } from "react";
+import { TextField } from "@mui/material";
 const Payment = () => {
   let count = 1;
 
   const [qty, setQty] = useState(1);
+
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("user"))
+  );
 
   const increment = () => {
     console.log("clicked!!");
@@ -12,54 +17,66 @@ const Payment = () => {
     setQty(qty + 1);
   };
   return (
-    <div>
-      <div class="card mycard card-c">
-        <div class="card-header">Price Details</div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            Price(1 item)<strong class="strong">Rs. 1,050</strong>
-          </li>
-          <li class="list-group-item">
-            Delivery Charges<strong class="strong">Free</strong>
-          </li>
-          <li class="list-group-item">
-            Total Payable<strong class="strong">Rs. 1,050</strong>
-          </li>
-        </ul>
-      </div>
-      <div></div>
+    <div className="container pt-5">
+      <div className="row">
+        <div className="col-md-8">
+          <div class="card">
+            <div class="card-header">Delivery Address</div>
+            <div class="card-body">
+              <h5 class="card-title">{currentUser.username}</h5>
+              <TextField
+                variant="outlined"
+                className="mt-4"
+                multiline
+                rows={3}
+              />
+              <p class="card-text">
+                Vishal Khand, Gomti Nagar, Lucknow (up)226010
+              </p>
+              <a href="#" class="btn btn-primary button">
+                Delivery Here
+              </a>
+              <a href="" class="edit">
+                Edit
+              </a>
+            </div>
+          </div>
 
-      <div class="card card-a">
-        <div class="card-header">Delivery Address</div>
-        <div class="card-body">
-          <h5 class="card-title">Janhavi Srivastava</h5>
-          <p class="card-text">Vishal Khand, Gomti Nagar, Lucknow (up)226010</p>
-          <a href="#" class="btn btn-primary button">
-            Delivery Here
-          </a>
-          <a href="" class="edit">
-            Edit
-          </a>
+          <div class="card mt-5">
+            <div class="card-header">Order Summary</div>
+            <img
+              class="imggg"
+              src="https://cdn.shopify.com/s/files/1/2980/5140/products/panel10_700x.jpg?v=1618827654"
+            ></img>
+            <div class="card-body card-b">
+              <h5 class="card-title">
+                Loom Solar Panel 10 watt - 12 volt for Mobile Charging
+              </h5>
+              <p class="card-text">Rs. 1,050</p>
+              <h3 className="quantity">Quantity : {qty}</h3>
+              <button className="btn btn-primary quantity" onClick={increment}>
+                {" "}
+                <i class="fa fa-chevron-up" aria-hidden="true"></i>{" "}
+              </button>
+              {/* <a href="#" class="btn btn-primary"></a> */}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div class="card card-bb">
-        <div class="card-header">Order Summary</div>
-        <img
-          class="imggg"
-          src="https://cdn.shopify.com/s/files/1/2980/5140/products/panel10_700x.jpg?v=1618827654"
-        ></img>
-        <div class="card-body card-b">
-          <h5 class="card-title">
-            Loom Solar Panel 10 watt - 12 volt for Mobile Charging
-          </h5>
-          <p class="card-text">Rs. 1,050</p>
-          <h3 className="quantity">Quantity : {qty}</h3>
-          <button className="btn btn-primary quantity" onClick={increment}>
-            {" "}
-            <i class="fa fa-chevron-up" aria-hidden="true"></i>{" "}
-          </button>
-          {/* <a href="#" class="btn btn-primary"></a> */}
+        <div className="col-md-4">
+          <div class="card">
+            <div class="card-header">Price Details</div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                Price(1 item)<strong class="strong">Rs. 1,050</strong>
+              </li>
+              <li class="list-group-item">
+                Delivery Charges<strong class="strong">Free</strong>
+              </li>
+              <li class="list-group-item">
+                Total Payable<strong class="strong">Rs. 1,050</strong>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
