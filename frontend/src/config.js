3577fd563d51f4bb878544e529c93ba0cec4d0a5
m-triple-m <triplem656@mail.com>
2022-12-13 00:29:48 +0530

    ..

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const app_config = {
-  backend_url: "http://localhost:5000",
+  // backend_url: "http://localhost:5000",
+  backend_url: "https://go-solar-rest-api.onrender.com",
 };
 
 export default app_config;
